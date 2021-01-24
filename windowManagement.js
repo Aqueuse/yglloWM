e804e0e02774d54f608f3623cf59e64b2e5b7872
Aqueuse <SR388-Aqueuse@users.noreply.github.com>
2021-01-24 20:24:21 +0100

    better the window management script (resize still in progress, move pretty ok)

--- windowManagement.js
+++ windowManagement.js
@@ -1,39 +1,79 @@
 var currentElement;
 
 var mouseDown = function (element) {
-    console.log(element.id);
     currentElement = element;
-    element.addEventListener('mousemove', resize);
-    element.addEventListener('mouseup', stopResize);
+    currentElement.parentElement.addEventListener('mouseup', stopEventListening, false);
+    if (currentElement.id === "headerID-"+currentElement.parentElement.id) {
+        currentElement.parentElement.addEventListener('mousemove', move, false);
+    }
+    else {
+        currentElement.parentElement.addEventListener('mousemove', resize, false);
+    }
+};
+
+var move = function(e) {
+    currentElement.parentElement.style.left = e.clientX - currentElement.offsetWidth/2 +"px";
+    currentElement.parentElement.style.top = e.clientY - (currentElement.offsetHeight) +"px";
 };
 
 var resize = function(e) {
-    if (currentElement.id === "headerID") {
+    if (currentElement.id === "borderLeftID-"+currentElement.parentElement.id) {
         currentElement.parentElement.style.left = e.clientX - currentElement.offsetWidth/2 +"px";
-        currentElement.parentElement.style.top = e.clientY - currentElement.offsetHeight/2 +"px";
+        currentElement.parentElement.style.width = (
+            e.clientX - currentElement.parentElement.style.left
+             ) + (currentElement.offsetWidth/2) + "px";
     }
 
-    if (currentElement.id === "borderLeftID") {
-        currentElement.parentElement.style.left = e.clientX - currentElement.offsetWidth/2 +"px";
-        currentElement.parentElement.style.width = e.clientX - currentElement.offsetWidth/2 +"px";
+    if (currentElement.id === "borderRightID-"+currentElement.parentElement.id) {
+        currentElement.parentElement.style.width = 
+        (e.clientX - (currentElement.parentElement.style.left))
+        + (currentElement.offsetWidth/4) + "px";
     }
 
-    if (currentElement.id === "borderRightID") {
-        currentElement.parentElement.style.width = e.clientX - currentElement.offsetWidth +"px";
-    }
-
-    if (currentElement.id === "borderTopID") {
+    if (currentElement.id === "borderTopID-"+currentElement.parentElement.id) {
         currentElement.parentElement.style.top = e.clientY - currentElement.offsetHeight/2 +"px";
         currentElement.parentElement.style.height = e.clientY - currentElement.offsetHeight/2 +"px";
     }
 
-    if (currentElement.id === "borderBottomID") {
+    if (currentElement.id === "borderBottomID-"+currentElement.parentElement.id) {
         currentElement.parentElement.style.height = e.clientY - currentElement.offsetHeight/2 +"px";
     }
 };
 
-var stopResize = function () {
-    console.log("stop resize");
-    currentElement.removeEventListener('mousemove', resize);
-    currentElement.removeEventListener('mouseup', stopResize);
+var stopEventListening = function (e) {
+    if (currentElement.id === "headerID-"+currentElement.parentElement.id) {
+        currentElement.parentElement.removeEventListener('mousemove', move, false);
+    }
+    else {
+        currentElement.parentElement.removeEventListener('mousemove', resize, false);
+    }
+    currentElement.parentElement.removeEventListener('mouseup', stopEventListening, false);        
 };
+
+function getUniqueID(complicatedTitle) {
+    var key = "-" + Math.random().toString(36).substring(2, 10);
+
+    var shortTitle = complicatedTitle.toLowerCase();
+    var arrayTitle = shortTitle.split(" ", 3);
+    var cleanTitle = [];
+
+    for (i = 0; i < arrayTitle.length; i++) {
+        var arrayWord = arrayTitle[i].split("");
+        for (j = 0; j < arrayWord.length; j++) {
+            if (isAlphaNumeric(arrayWord[j]))
+                cleanTitle.push(arrayWord[j]);
+        }
+    }
+    return cleanTitle.join("") + key;
+}
+
+function isAlphaNumeric(char) {
+    var code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+        return false;
+    }
+    else {
+        return true;
+    }
+}
--- yglloWindow.js
+++ yglloWindow.js
@@ -7,33 +7,6 @@ class yglloWindow extends HTMLElement {
         // Create a shadow root
         const shadow = this.attachShadow({ mode: 'open' });
 
-        // Create div
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class', 'wrapper');
-        wrapper.setAttribute('id', title);
-
-        const borderLeft = document.createElement('div');
-        borderLeft.setAttribute('class', 'windowBorder borderLeft');
-        borderLeft.setAttribute('onmousedown', 'mouseDown(this)');
-        borderLeft.setAttribute('id', 'borderLeftID');
-        const borderRight = document.createElement('div');
-        borderRight.setAttribute('class', 'windowBorder borderRight');
-        borderRight.setAttribute('onmousedown', 'mouseDown(this)');
-        borderRight.setAttribute('id', 'borderRightID');
-        const borderTop = document.createElement('div');
-        borderTop.setAttribute('class', 'windowBorder borderTop');
-        borderTop.setAttribute('onmousedown', 'mouseDown(this)');
-        borderTop.setAttribute('id', 'borderTopID');
-        const borderBottom = document.createElement('div');
-        borderBottom.setAttribute('class', 'windowBorder borderBottom');
-        borderBottom.setAttribute('onmousedown', 'mouseDown(this)');
-        borderBottom.setAttribute('id', 'borderBottomID');
-
-        const header = document.createElement('div');
-        header.setAttribute('class', 'windowHeader');
-        header.setAttribute('onmousedown', 'mouseDown(this)');
-        header.setAttribute('id', 'headerID');
-
         // Create some CSS to apply to the shadow dom
         const style = document.createElement('style');
         var generalImport = "@import './yglloWM.css';\n";
@@ -45,6 +18,35 @@ class yglloWindow extends HTMLElement {
 
         var wrapperStyle=generalImport+".wrapper {\n"+positionAbsolute+left+top+width+height+"\n}\n";
         style.textContent=wrapperStyle;
+
+        var uniqueID = getUniqueID(title);
+
+        // Create div
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class', 'wrapper');
+        wrapper.setAttribute('id', uniqueID);
+
+        const borderLeft = document.createElement('div');
+        borderLeft.setAttribute('class', 'windowBorder borderLeft');
+        borderLeft.setAttribute('onmousedown', 'mouseDown(this)');
+        borderLeft.setAttribute('id', 'borderLeftID-'+uniqueID);
+        const borderRight = document.createElement('div');
+        borderRight.setAttribute('class', 'windowBorder borderRight');
+        borderRight.setAttribute('onmousedown', 'mouseDown(this)');
+        borderRight.setAttribute('id', 'borderRightID-'+uniqueID);
+        const borderTop = document.createElement('div');
+        borderTop.setAttribute('class', 'windowBorder borderTop');
+        borderTop.setAttribute('onmousedown', 'mouseDown(this)');
+        borderTop.setAttribute('id', 'borderTopID-'+uniqueID);
+        const borderBottom = document.createElement('div');
+        borderBottom.setAttribute('class', 'windowBorder borderBottom');
+        borderBottom.setAttribute('onmousedown', 'mouseDown(this)');
+        borderBottom.setAttribute('id', 'borderBottomID-'+uniqueID);
+
+        const header = document.createElement('div');
+        header.setAttribute('class', 'windowHeader');
+        header.setAttribute('onmousedown', 'mouseDown(this)');
+        header.setAttribute('id', 'headerID-'+uniqueID);
 
         // Attach the created elements to the shadow dom
         wrapper.appendChild(borderLeft);
