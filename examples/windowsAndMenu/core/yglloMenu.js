86504fed090ff0a5b950aaa6934970caafcd27ac
Aqueuse <SR388-Aqueuse@users.noreply.github.com>
2021-02-03 21:08:22 +0100

    advancing the menu : submenu positionning and relative inline

--- examples/windowsAndMenu/core/yglloMenu.js
+++ examples/windowsAndMenu/core/yglloMenu.js
@@ -7,7 +7,7 @@ var item;
 ////////////////// The main menu class /////////////////////
 
 class yglloMainMenu extends HTMLElement {
-    constructor(top) {
+    constructor(top, menuName) {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
 
@@ -21,6 +21,7 @@ class yglloMainMenu extends HTMLElement {
 
         menuParent = document.createElement('div');
         menuParent.setAttribute('class', 'menuParent');
+        menuParent.setAttribute('name',menuName);
         menuParent.setAttribute('id', uniqueID);
 
         // this background allow us to organize things simply with inline elements
@@ -29,15 +30,16 @@ class yglloMainMenu extends HTMLElement {
         // menuBarBackground.setAttribute('style', "position: absolute; left:0px; width:100%;");
         // menuParent.appendChild(menuBarBackground);
 
-        var verticalPosition = "top : " + top + "px;";
-        var horizontalPosition = "left : 0px;";
+        var verticalPosition = "top:" + top + "px;";
+        var horizontalPosition = "left:0px;";
         var width = "width:100%;";
         var height = "height:5vh;";
-        var parentStyle = "position : absolute;" +
+        var parentStyle = "position:absolute;" +
             verticalPosition +
             horizontalPosition +
             width +
-            height;
+            height+
+            "display:inline;";
  
         menuParent.setAttribute('style', parentStyle);
 
@@ -45,7 +47,7 @@ class yglloMainMenu extends HTMLElement {
         shadow.append(menuParent);
 
         this.addItem = function(name, onclick) {
-            item = new yglloItem(verticalPosition, horizontalPosition, name, onclick);
+            item = new yglloItem("mainMenu", name, onclick);
             menuParent.append(item);
         }
     }
@@ -61,7 +63,7 @@ class yglloSubMenu extends HTMLElement {
         const shadow = this.attachShadow({ mode: 'open' });
 
         this.setAttribute('name', menuName);
-        uniqueID = getUniqueID("submenu-");
+        this.setAttribute('class', 'subMenu');
 
         // generate CSS to apply to the menu
         const style = document.createElement('style');
@@ -69,25 +71,26 @@ class yglloSubMenu extends HTMLElement {
         style.textContent = generalImport;
 
         subMenu = document.createElement('div');
-        subMenu.setAttribute('class', 'subMenu');
-        subMenu.setAttribute('id', uniqueID);
 
-        var parentTop = document.getElementsByName(parentMenuName)[0].shadowRoot.childNodes[1].style.top;
-        console.log(parentTop);
-        var verticalPosition = "top : "+parentTop+";";
-        var horizontalPosition = "left : 0px;";
+        var parentTop = parseInt(document.getElementsByName(parentMenuName)[0].shadowRoot.childNodes[1].style.top, 10);
+        var parentVhHeight = document.getElementsByName(parentMenuName)[0].shadowRoot.childNodes[1].style.height;
+        var parentHeight = parseInt(parentVhHeight,10)*6.57;
 
-        var wrapperStyle = "position : absolute;" +
+        var verticalPosition = "top:"+parentTop+parentHeight+"px;";
+        var horizontalPosition = "left:0px;";
+
+        var wrapperStyle = "position:absolute;" +
             verticalPosition +
-            horizontalPosition;
+            horizontalPosition+
+            "visibility:hidden;";
  
-        subMenu.setAttribute('style', wrapperStyle);
+        this.setAttribute('style', wrapperStyle);
 
         shadow.appendChild(style);
         shadow.append(subMenu);
 
         this.addItem = function(name, onclick) {
-            item = new yglloItem(verticalPosition, horizontalPosition, name, onclick);
+            item = new yglloItem("subMenu", name, onclick);
             subMenu.append(item);
         }
     }
@@ -98,22 +101,25 @@ customElements.define('ygllo-submenu', yglloSubMenu);
 ///////////////////////// the item class ////////////////////////
 
 class yglloItem extends HTMLElement {
-    constructor(parentTop, parentLeft, name, onclick) {
+    constructor(parentType, itemName, onclick) {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
 
-        uniqueID = getUniqueID("item-" + name);
+        uniqueID = getUniqueID("item-" + itemName);
 
         const style = document.createElement('style');
         var generalImport = "@import './CSS/yglloWM.css';\n";
         style.textContent = generalImport;
 
         item = document.createElement('span');
+        if (parentType=="subMenu") {
+            item.setAttribute('style','display:block; vertical-align:middle;');
+        }
+
         item.setAttribute('id', uniqueID);
         item.setAttribute('class', 'menuItem');
-        item.innerHTML = name;
+        item.innerHTML = itemName;
         item.setAttribute('onclick', onclick);
-        item.setAttribute('style',"position:relative;");
 
         shadow.appendChild(style);
         shadow.appendChild(item);
@@ -125,12 +131,16 @@ customElements.define('ygllo-item', yglloItem);
 ///////////////// the functions to create and click the menu  /////////////////
 
 function openSubMenu(itemClicked, subMenuName) {
-    console.log("Hi, I will open this subMenu\nI am "+itemClicked.id);
     // we will display the submenu and move it at the top/right corner of the item
-    console.log(itemClicked);
-    console.log(document.getElementsByName(subMenuName)[0].shadowRoot.childNodes[1].style.top);
+//    console.log(itemClicked);
+    document.getElementsByName(subMenuName)[0].style.visibility = "visible";
+    console.log(document.getElementsByName(subMenuName));
 }
 
 function openWindow(windowName) {
-    console.log(document.getElementsByName(windowName)[0].shadowRoot.childNodes[1].style.top);
+    // find the parent menu
+    var parentTop = document.getElementsByName(windowName)[0].shadowRoot.childNodes[1].style.top;
+    var parentHeight = document.getElementsByName(windowName)[0].shadowRoot.childNodes[1].style.height;
+
+    console.log(parentHeight);
 }
